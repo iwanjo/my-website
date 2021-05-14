f91ee9dda015dd6f18660848c38c49a2b5e0b75c
iwanjo <kamanuian@gmail.com>
2021-05-14 09:18:23 +0200

    website preloader config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 import './App.css';
+import Home from './Home';
+import { Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  // fake authentication Promise
+  authenticate(){
+    return new Promise(resolve => setTimeout(resolve, 2000)) // 2 seconds
+  }
+
+  componentDidMount(){
+    this.authenticate().then(() => {
+      const ele = document.getElementById('ipl-progress-indicator')
+      if(ele){
+        // fade out
+        ele.classList.add('available')
+        setTimeout(() => {
+          // remove from DOM
+          ele.outerHTML = ''
+        }, 2000)
+      }
+    })
+  }
+
+  render() {
+    return (
+      <>
+      <Route exact path="/" component={Home}></Route>
+      </>
+
+    );
+  }
 }
 
 export default App;
+
---
+++ src/Home.js
@@ -0,0 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect } from 'react'
+
+import Fade from 'react-reveal/Fade';
+import ScrollToTop from "react-scroll-to-top";
+import { Link }	from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col';
+
+
+function Home() {
+    
+
+    return(
+        <>
+            <h1>Hello</h1>
+            <p>Lorem</p>
+
+        </>
+    )
+
+}
+
+export default Home;
