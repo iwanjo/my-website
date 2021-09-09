5625939b52f7505a2d5a1d3a565e0f76e43344a1
iwanjo <kamanuian@gmail.com>
2021-09-09 09:36:44 +0200

    DiscoverKenya-Challenge

--- src/projects/DiscoverKenya.js
+++ src/projects/DiscoverKenya.js
@@ -14,18 +14,68 @@ export default function DiscoverKenya() {
       </Helmet>
       <ProjectsNavbar />
 
-      <section className={styles.heroSection}>
+      <section className={styles.heroSection} id="section1">
         <div className={styles.heroContainer}>
           <div className={styles.leftContainer}>
-          <img src="../assets/dk-logo.svg" alt="Discover Kenya Logo" className={styles.logo} />
+            <img
+              src="../assets/dk-logo.svg"
+              alt="Discover Kenya Logo"
+              className={styles.logo}
+            />
             <h1 className={styles.projectTitle}>Discover Kenya</h1>
-            <p className={styles.projectText}>Connecting users to beautiful photos of Kenya, while creating a community of visual contributors.</p>
-
+            <p className={styles.projectText}>
+              Connecting users to beautiful photos of Kenya, while creating a
+              community of visual contributors. A hub for individuals and
+              businesses to share Kenya's magic.
+            </p>
+            <button className={styles.tags}>In Development</button>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <button className={styles.tags}>Mobile Application</button>
           </div>
 
-          <div className={styles.rightContainer}></div>
+          <div className={styles.rightContainer}>
+            <img
+              src="../assets/prototype.png"
+              loading="eager"
+              alt="Discover Kenya Homepage"
+              className={styles.protoImg}
+            />
+          </div>
         </div>
-        <div class={styles.waves}></div>
+      </section>
+
+      <section className={styles.challenge} id="section2">
+        <div className={styles.challengeDiv}>
+          <h1 className={styles.challengeTitle}>The Challenge</h1>
+          <p className={styles.challengeText}>
+            As a result of the COVID-19 virus and the various safety measures
+            implemented, numerous areas of economic activity were dealt a severe
+            blow. At the height of the pandemic in the spring and summer of
+            2020, many countries imposed bans on international travel,
+            lockdowns, physical distancing and curfews, which had harsh economic
+            consequences. One area that stood out to me is the tourism industry
+            in Kenya. The country is one of the leading tourist destinations in
+            Africa, offering a wide range of attractions such as pristine
+            beaches, safaris, deserts and more. 
+            <br />
+            <br />
+            Due to the pandemic prevention
+            measures and their far-reaching consequences, the industry suffered
+            massive job losses, closure of local travel and tourism agencies,
+            and revenue losses. Tourism arrivals in Kenya dropped by more than
+            70% in Kenya during the first few months of 2020, and the country
+            lost more than $1 Billion in revenue from annual tourism
+            earnings.
+          </p>
+
+          <div className={styles.imgDiv}>
+            <img
+              src="../assets/tourist-graph.jpeg"
+              className={styles.challengeImg}
+              alt="Graph showcasing tourism numbers differences between 2019 and 2020."
+            />
+          </div>
+        </div>
       </section>
     </>
   );
--- src/projects/ProjectsNavbar.js
+++ src/projects/ProjectsNavbar.js
@@ -1,23 +1,82 @@
-import React from 'react';
-import styles from '../css-modules/Navbar.module.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import styles from "../css-modules/Navbar.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import { LinkContainer } from 'react-router-bootstrap';
+import { LinkContainer } from "react-router-bootstrap";
+import { Link as Scroll } from "react-scroll";
 
 function ProjectsNavbar() {
-    return (
-        <>
-            <Navbar collapseOnSelect expand="lg" sticky="top" className={styles.topNav}>
-            <LinkContainer to="/">
-                <Navbar href="#home" className={styles.brand}>
-                    <img src="/ian-logo.png" alt="Ian's Avatar" className={styles.avatar}></img>
-                </Navbar>
-            </LinkContainer>
-                
-            </Navbar>
-        </>
-    )
+  return (
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        sticky="top"
+        className={styles.topNav}
+      >
+        <LinkContainer to="/">
+          <Navbar href="#home" className={styles.brand}>
+            <img
+              src="/ian-logo.png"
+              alt="Ian's Avatar"
+              className={styles.avatar}
+            ></img>
+          </Navbar>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto"></Nav>
+          <Nav className={styles.navLinksArea}>
+            <Scroll
+              activeClass="active"
+              to="section1"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              <Nav.Link className={styles.navLink}>
+                Project Home
+              </Nav.Link>
+            </Scroll>
+
+            <Scroll
+            activeClass="active"
+            to="section2"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            >
+              <Nav.Link className={styles.navLink}>Challenge</Nav.Link>
+            </Scroll>
+            <Scroll
+              activeClass="active"
+              to="section3"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              <Nav.Link className={styles.navLink}>HCD Process</Nav.Link>
+            </Scroll>
+
+            <Scroll
+              activeClass="active"
+              to="section4"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              <Nav.Link className={styles.navLink}>Implementation</Nav.Link>
+            </Scroll>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
 }
 
 export default ProjectsNavbar;
